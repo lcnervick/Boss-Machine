3b68e7e62aa3b0b57f9e41a86fb1cf88e2861e83
leif@synapticsoftware.net <leif@synapticsoftware.net>
2023-09-06 08:32:16 -0600

    move index to public folder

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+const apiRouter = require('./server/api');
+
 const app = express();
 
 module.exports = app;
@@ -9,17 +14,20 @@ module.exports = app;
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
-
+app.use(cors());
 
 // Add middware for parsing request bodies here:
-
+app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
-const apiRouter = require('./server/api');
-
+app.use('/api', apiRouter);
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
-
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+    console.log(`Serving files from ${__dirname}${path.sep}public`)
+    app.use(express.static(`${__dirname}${path.sep}public`));
+  });
 }
